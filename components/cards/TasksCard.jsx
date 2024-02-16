856337cba0d6d523c5fe29fe454fcb657760b7dd
TJ Klimoski <tjklimoski@gmail.com>
2024-02-15 21:09:04 -0500

    Create TasksCard

--- components/cards/TasksCard.jsx
+++ components/cards/TasksCard.jsx
@@ -1,3 +1,13 @@
 export default function TasksCard() {
-  return <div>TasksCard</div>;
+  return (
+    <div className="card mx-4">
+      <div className="card-header">Tasks</div>
+      <div className="card-body">
+        <ul>
+          <li>Email Daniel</li>
+          <li>Check bug</li>
+        </ul>
+      </div>
+    </div>
+  );
 }

c13f5f669072b078e40dfed9b8016188413d0f55
TJ Klimoski <tjklimoski@gmail.com>
2024-02-15 23:25:31 -0500

    stylize formatDateTime function to place a "@" between the date and time

--- util/dateFormatter.js
+++ util/dateFormatter.js
@@ -13,10 +13,14 @@ export function formatDateTime(date_input) {
   // Check if date is a valid date object. if not valid, return input value
   if (date instanceof Date && isNaN(date)) return date_input;
 
-  return Intl.DateTimeFormat(undefined, {
+  const formattedDateTime = Intl.DateTimeFormat(undefined, {
     dateStyle: "medium",
     timeStyle: "short",
   }).format(date);
+
+  const styledDateTime = formattedDateTime.replace(",", " @");
+
+  return styledDateTime;
 }
 
 export const today = formatDate(new Date());

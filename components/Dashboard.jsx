5781b6e2e6ddcb2e7bda4e4c8fc23ceae5856fb7
TJ Klimoski <tjklimoski@gmail.com>
2024-02-15 20:57:41 -0500

    Change apostrophe to &apos;

--- components/Dashboard.jsx
+++ components/Dashboard.jsx
@@ -4,7 +4,7 @@ export default function Dashboard({ auth }) {
   return (
     <div className="container-xxl">
       <header className="d-flex justify-content-between my-4">
-        <h1 className="me-4">{auth.user.name}'s Dashboard </h1>
+        <h1 className="me-4">{auth.user.name}&apos;s Dashboard </h1>
         <span className="fw-light">Jan 5, 2024</span>
       </header>
       <TasksCard />
